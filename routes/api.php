9dcaf7d4448506a0214d70289c4d7cfa3931c3fe
Layal Dougho <layaldogho9@gmail.com>
2023-08-26 22:14:44 +0300

    middleware

--- routes/api.php
+++ routes/api.php
@@ -24,37 +24,45 @@ Route::controller(AuthController::class)->group(function () {
 });
 /////////////////////////////////////////////
 
+Route::prefix('user')->middleware('auth')->group(function(){
+    
 //Category Routes
 
-Route::get('categories',[CategoryController::class,'index'])->middleware('auth:api');
-Route::get('categories/check/name',[CategoryController::class,'checkName'])->middleware('auth:api');
-Route::get('categories/store',[CategoryController::class,'store'])->middleware('auth:api');
-Route::get('categories/{id}/show',[CategoryController::class,'show'])->middleware('auth:api');
-Route::get('categories/check/edit/name',[CategoryController::class,'checkEditName'])->middleware('auth:api');
-Route::get('categories/update',[CategoryController::class,'update'])->middleware('auth:api');
-Route::get('categories/destroy',[CategoryController::class,'destroy'])->middleware('auth:api');
+    Route::get('categories',[CategoryController::class,'index'])->middleware('is_admin');
+    Route::get('categories/check/name',[CategoryController::class,'checkName'])->middleware('is_admin');
+    Route::get('categories/store',[CategoryController::class,'store'])->middleware('is_admin');
+    Route::get('categories/{id}/show',[CategoryController::class,'show'])->middleware('is_admin');
+    Route::get('categories/check/edit/name',[CategoryController::class,'checkEditName'])->middleware('is_admin');
+    Route::get('categories/update',[CategoryController::class,'update'])->middleware('is_admin');
+    Route::get('categories/destroy',[CategoryController::class,'destroy'])->middleware('is_admin');
 
 
-//Post Routes
+    //Post Routes
 
-Route::get('post',[PostController::class,'index'])->middleware('auth:api');
-Route::post('post/check/title',[PostController::class,'checkTitle'])->middleware('auth:api');
-Route::post('post/check/category',[PostController::class,'checkCategory'])->middleware('auth:api');
-Route::post('post/check/content',[PostController::class,'checkcontent'])->middleware('auth:api');
-Route::post('post/store',[PostController::class,'store'])->middleware('auth:api');
-Route::get('post/{id}/show',[PostController::class,'show']);
-Route::post('post/update',[PostController::class,'update'])->middleware('auth:api');
-Route::post('post/destroy',[PostController::class,'destroy'])->middleware('auth:api');
-Route::get('post/{id}/tag',[PostController::class,'tag']);
+    Route::get('post',[PostController::class,'index'])->middleware('check_user');
+    Route::post('post/check/title',[PostController::class,'checkTitle'])->middleware('check_user');
+    Route::post('post/check/category',[PostController::class,'checkCategory'])->middleware('check_user');
+    Route::post('post/check/content',[PostController::class,'checkcontent'])->middleware('check_user');
+    Route::post('post/store',[PostController::class,'store'])->middleware('check_user');
+    Route::get('post/{id}/show',[PostController::class,'show'])->middleware('check_user');
+    Route::post('post/update',[PostController::class,'update'])->middleware('check_user');
+    Route::post('post/destroy',[PostController::class,'destroy'])->middleware('check_user');
+    Route::get('post/{id}/tag',[PostController::class,'tag'])->middleware('check_user');
 
+    
 //Tag Routes
 
-Route::get('tag',[CommentController::class,'index'])->middleware('auth:api');
-Route::post('tag/check/tag',[CommentController::class,'checkTag'])->middleware('auth:api');
-Route::post('tag/check/post',[CommentController::class,'checkPost'])->middleware('auth:api');
-Route::post('tag/store',[CommentController::class,'store'])->middleware('auth:api');
-Route::get('tag/{id}/show',[CommentController::class,'show']);
-Route::post('tag/{id}/update',[CommentController::class,'update'])->middleware('auth:api');
-Route::post('tag/{id}/destroy',[CommentController::class,'destroy'])->middleware('auth:api');
+    Route::get('tag',[CommentController::class,'index'])->middleware('is_admin');
+    Route::post('tag/check/tag',[CommentController::class,'checkTag'])->middleware('check_user');
+    Route::post('tag/check/post',[CommentController::class,'checkPost'])->middleware('is_admin');
+    Route::post('tag/store',[CommentController::class,'store'])->middleware('check_user');
+    Route::get('tag/{id}/show',[CommentController::class,'show'])->middleware('check_user');
+    Route::post('tag/{id}/update',[CommentController::class,'update'])->middleware('check_user');
+    Route::post('tag/{id}/destroy',[CommentController::class,'destroy'])->middleware('is_admin');
+
+
+});
+
+
 
 
